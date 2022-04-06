3c3a0d39e2219aee12d0244d67daf0c2522034f8
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2022-04-06 10:35:45 +1000

    Added more currency pairs

--- src/currencyData.js
+++ src/currencyData.js
@@ -2,11 +2,26 @@
 
 const currencyList = [
   "AED-UAE Dirham",
+  "ARS-Argentine Peso",
   "AUD-Australian Dollar",
-  "INR-Indian Rupee",
-  "USD-US Dollar",
+  "BHD-Bahraini Dinar",
+  "BRL-Brazilian Real",
   "CAD-Canadian Dollar",
+  "CHF-Swiss franc",
+  "CNY-Chinese yuan",
+  "EGP-Egyptian Pound",
+  "EUR-Euro",
+  "GBP-British Pund Sterling",
+  "HKD-Hong Kong Dollar",
+  "IDR-Indonesian Rupiah",
+  "INR-Indian Rupee",
+  "JPY-Japanese Yen",
+  "KRW-South Korean Won",
   "NZD-New Zealand Dollar",
+  "PHP-Philippine Peso",
+  "PKR-Pakistani Rupee",
+  "SGD-Singapore Dollar",
+  "USD-US Dollar",
 ];
 
 export default currencyList;
