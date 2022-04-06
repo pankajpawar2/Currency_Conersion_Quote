8c29851b9839d1b50c11090d4e80649d29ea2785
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2022-04-06 15:34:24 +1000

    Added styling to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 export default function Header() {
   return (
-    <nav className="w-80 m-lg-2 p-1 display-5 bg-info text-white">
-      <p className="font-weight-bold text-center">OFX - GET A QUOTE</p>
+    <nav className="w-80 m-lg-2 p-1 display-5 bg-secondary text-dark">
+      <p className="font-weight-bold text-center m-2">
+        OFX - GET A QUICK QUOTE
+      </p>
     </nav>
   );
 }
